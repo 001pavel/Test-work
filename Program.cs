9b6ad8693f84969799bab246599e3e9be1d4294d
PavelKrak <tropicpost@mail.ru>
2022-10-01 10:17:10 +0300

    Написал метод вывода строкового массива на экран

--- Program.cs
+++ Program.cs
@@ -2,3 +2,16 @@
 string[] array2 = {"1234", "1567", "-2", "computer science"};
 string[] array3 = {"Russia", "Denmark", "Kazan"};
 string[] array4 = {"Ru", "Den4", "Su"};
+
+void PrintStringArray(string[] array)
+{
+    int length = array.Length;
+    string result = String.Empty;
+    for (int i = 0; i < length; i++)
+        {
+            result = result + array[i] + ", ";
+        }
+    if (result.Length > 1) result = result.Substring(0, result.Length - 2);
+    Console.Write("[" + result + "]");
+}
+
